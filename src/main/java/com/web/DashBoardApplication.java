06731e9b887420b30fb05f8e38c40efd844e0b71
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-11-08 10:10:44 +0900

    회원가입 및 Auth Client 연결 추가

--- src/main/java/com/web/DashBoardApplication.java
+++ src/main/java/com/web/DashBoardApplication.java
@@ -3,11 +3,24 @@ package com.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.event.EventListener;
+
+import java.util.Date;
+import java.util.TimeZone;
 
 @Slf4j
 @SpringBootApplication
+@EnableFeignClients
 public class DashBoardApplication {
     public static void main(String[] args) {
         SpringApplication.run(DashBoardApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.info("Spring boot application running in Asia/Seoul timezone : " + new Date());
     }
 }
---
+++ src/main/java/com/web/client/HttpClient.java
@@ -0,0 +1,4 @@
+package com.web.client;
+
+public interface HttpClient {
+}
